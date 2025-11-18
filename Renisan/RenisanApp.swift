a8812efe874e07e22c4fd2c9e95d6b4b9d7ed66c
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2025-11-18 16:23:28 -0600

    Initial Commit

---
+++ Renisan/RenisanApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenisanApp.swift
+//  Renisan
+//
+//  Created by erhan gumus on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RenisanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
